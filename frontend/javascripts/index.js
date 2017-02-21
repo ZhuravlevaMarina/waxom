329f0dabb100e3f86e71d2b476b56d7611a0aeb7
ZhuravlevaMarina <marinajuravleva28gmail.com>
2017-02-21 20:46:25 +0200

    next

--- frontend/javascripts/index.js
+++ frontend/javascripts/index.js
@@ -2,13 +2,14 @@ requirejs.config({
     baseUrl: './public/js'
 });
 
-define(["jquery", "domReady", "swiper.min"], function($) {
+define(["jquery", "domReady", "swiper.min", "isotope.pkgd.min"], function($) {
     "use strict";
 
     var Homepage = {
         init: function () {
             this.carousel();
             this.carouselBottom();
+            this.tabsBox();
         },
         carousel: function () {
             var swiper = new Swiper('.swiper-container', {
@@ -27,6 +28,24 @@ define(["jquery", "domReady", "swiper.min"], function($) {
                 nextButton: '.swiper-button-next',
                 prevButton: '.swiper-button-prev',
                 spaceBetween: 30
+            });
+        },
+        tabsBox: function() {
+            // init Isotope
+            var grid =  new Isotope('.isotope');
+
+            // bind filter button click
+            $('.button-group').on( 'click', 'button', function() {
+                var filterValue = $( this ).attr('data-filter');
+                grid.arrange({ filter: filterValue });
+            });
+            // change is-checked class on buttons
+            $('.js-radio-button-group').each( function( i, buttonGroup ) {
+                var $buttonGroup = $( buttonGroup );
+                $buttonGroup.on( 'click', 'button', function() {
+                    $buttonGroup.find('.is-checked').removeClass('is-checked');
+                    $( this ).addClass('is-checked');
+                });
             });
         }
     };
